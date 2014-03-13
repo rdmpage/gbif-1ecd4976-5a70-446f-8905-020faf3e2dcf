e50dced4e5055284c97dbb3a57757591bd3ffb89
Roderic Page <rdmpage@gmail.com>
2014-03-13 16:23:30 +0000

    Data

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ GBIF dataset for distribution of Banza katydids, using data from:
 
 Shapiro, L. H., Strazanac, J. S., & Roderick, G. K. (2006, October). Molecular phylogeny of Banza (Orthoptera: Tettigoniidae), the endemic katydids of the Hawaiian Archipelago. Molecular Phylogenetics and Evolution. Elsevier BV. [doi:10.1016/j.ympev.2006.04.006](http://dx.doi.org/10.1016/j.ympev.2006.04.006)
 
-## Step 1
+## Step 1 Create dataset on GBIF
 
 Create a dataset on GBIF using registry API. The **owningOrganizationKey** is the publisher UUID that you see in the link to the publisher page: http://www.gbif.org/publisher/92f51af1-e917-49bc-a8ed-014ed3a77bec. You also need a secret **installationKey** provided by GBIF, and you also need to authenticate the call using your GBIF portal username and password.
 
@@ -28,3 +28,21 @@ RESPONSE
 ```
 
 We now have a UUID (1ecd4976-5a70-446f-8905-020faf3e2dcf) for the dataset, which lives here: http://www.gbif.org/dataset/1ecd4976-5a70-446f-8905-020faf3e2dcf
+
+## Step 2 Create and validate Darwin Core archive
+
+Now we need to create the Darwin Core archive. I grabbed the data from Appendix A of [doi:10.1016/j.ympev.2006.04.006](http://dx.doi.org/10.1016/j.ympev.2006.04.006) and pasted it into a Microsoft Excel spreadsheet. I then wrote various simple formulae to transform the data (e.g., convert latitude and longitudes such as 23°03′44′′N 161°55′34′′W into 23.06222222 -161.9261111. The final data set is exported to a tab-delimited text file occurrences.text.
+
+I then generated a meta.xml file, and finally the Darwin Core Archive (DwC-A) (which is simply a zip file):
+
+zip banza.zip meta.xml occurrences.txt
+
+Next we need to check that the DwC-A file is valid using the [Darwin Core Archive Validator](http://tools.gbif.org/dwca-validator/).
+
+Step 
+
+Step 3
+
+Now we need to upload the data. I created an endpoint for the data
+
+
---
+++ dwca.php
@@ -0,0 +1,117 @@
+<?php
+
+// Create Darwin Core Archive for simple occurrence data
+
+error_reporting(E_ALL);
+
+//--------------------------------------------------------------------------------------------------
+$dwc_fields = array(
+	
+	// a
+	'associatedReferences'		=> 'http://rs.tdwg.org/dwc/terms/associatedReferences',
+	'associatedSequences'		=> 'http://rs.tdwg.org/dwc/terms/associatedSequences',
+	
+	// b
+	'basisOfRecord'				=> 'http://rs.tdwg.org/dwc/terms/basisOfRecord',
+	'bibliographicCitation'		=> 'http://purl.org/dc/terms/bibliographicCitation',
+
+	// c
+	'catalogNumber' 			=> 'http://rs.tdwg.org/dwc/terms/catalogNumber',
+	'collectionCode'			=> 'http://rs.tdwg.org/dwc/terms/collectionCode',
+	'continent'					=> 'http://rs.tdwg.org/dwc/terms/continent',
+	'county'					=> 'http://rs.tdwg.org/dwc/terms/county',
+	'country'					=> 'http://rs.tdwg.org/dwc/terms/country',
+	'countryCode'				=> 'http://rs.tdwg.org/dwc/terms/countryCode',
+	
+	// d
+	'datasetID'					=> 'http://rs.tdwg.org/dwc/terms/datasetID',
+    'decimalLatitude' 			=> 'http://rs.tdwg.org/dwc/terms/decimalLatitude',
+    'decimalLongitude' 			=> 'http://rs.tdwg.org/dwc/terms/decimalLongitude',
+	
+	
+	//i
+	'institutionCode' 			=> 'http://rs.tdwg.org/dwc/terms/institutionCode',
+	'island'					=> 'http://rs.tdwg.org/dwc/terms/island',
+	'islandGroup'				=> 'http://rs.tdwg.org/dwc/terms/islandGroup',
+	
+	// l
+	'location'					=> 'http://rs.tdwg.org/dwc/terms/locality',
+	'locality'					=> 'http://rs.tdwg.org/dwc/terms/locality',
+	
+	// s
+	'scientificName'			=> 'http://rs.tdwg.org/dwc/terms/scientificName',
+	'scientificNameAuthorship'	=> 'http://rs.tdwg.org/dwc/terms/scientificNameAuthorship',
+	'scientificNameID'			=> 'http://rs.tdwg.org/dwc/terms/scientificNameID',
+	'stateProvince'				=> 'http://rs.tdwg.org/dwc/terms/stateProvince',
+	
+	// t
+	'taxonID'					=> 'http://rs.tdwg.org/dwc/terms/taxonID',
+	'type'						=> 'http://purl.org/dc/terms/type',
+	'typeStatus'				=> 'http://rs.tdwg.org/dwc/terms/typeStatus',
+	
+	// v
+	'verbatimCoordinates' 		=> 'http://rs.tdwg.org/dwc/terms/verbatimCoordinates',
+	'verbatimLatitude'			=> 'http://rs.tdwg.org/dwc/terms/verbatimLatitude',
+	'verbatimLocality'			=> 'http://rs.tdwg.org/dwc/terms/verbatimLocality',
+	'verbatimLongitude'			=> 'http://rs.tdwg.org/dwc/terms/verbatimLongitude',
+	
+	// w
+	'waterBody'					=> 'http://rs.tdwg.org/dwc/terms/waterBody'
+);
+
+$delimiter = "\t";
+
+$occurrence_filename 	= 'occurrences.txt';
+
+$file = fopen($occurrence_filename, "r");
+
+// Read first row to get headings
+$row = trim(fgets($file));
+
+$occurrence_header = explode("\t", $row);
+
+//print_r($occurrence_header);
+
+// meta
+$metadata = new DomDocument('1.0', 'UTF-8');
+
+// Nice output
+$metadata->preserveWhiteSpace = false;
+$metadata->formatOutput = true;	
+
+$archive = $metadata->appendChild($metadata->createElement('archive'));
+$archive->setAttribute('xmlns', 	'http://rs.tdwg.org/dwc/text/');
+$archive->setAttribute('xmlns:xsi', 	'http://www.w3.org/2001/XMLSchema-instance');
+$archive->setAttribute('xsi:schemaLocation', 'http://rs.tdwg.org/dwc/text/  http://rs.tdwg.org/dwc/text/tdwg_dwc_text.xsd');
+
+// occurrences
+$core = $archive->appendChild($metadata->createElement('core'));
+
+$core->setAttribute('encoding', 			'UTF-8');
+$core->setAttribute('fieldsTerminatedBy', 	'\t');
+$core->setAttribute('linesTerminatedBy', 	'\n');
+$core->setAttribute('ignoreHeaderLines',  	'1');
+$core->setAttribute('rowType',  			'http://rs.tdwg.org/dwc/terms/Occurrence');
+
+$files = $core->appendChild($metadata->createElement('files'));
+$location = $files->appendChild($metadata->createElement('location'));
+$location->appendChild($metadata->createTextNode($occurrence_filename));
+
+$id = $core->appendChild($metadata->createElement('id'));
+$id->setAttribute('index', 	'0');
+
+$index = 0;
+foreach ($occurrence_header as $header)
+{
+	if ($index != 0)
+	{
+		$field = $core->appendChild($metadata->createElement('field'));
+		$field->setAttribute('index', 	$index);
+		$field->setAttribute('term', 	$dwc_fields[$header]);
+	}
+	$index++;
+}
+
+echo $metadata->saveXML()
+
+?>
